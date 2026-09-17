9578228b2768c3b72557033cf8c208d4579fedf6
poojahina <poojabhatnagar18@gmail.com>
2026-09-17 20:59:34 +0530

    bekkar

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -4,6 +4,8 @@ using Backend.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -20,6 +22,12 @@ builder.Services.AddScoped<ReviewerAgent>();
 builder.Services.AddScoped<ReviewOrchestrator>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors();
 app.MapControllers();
